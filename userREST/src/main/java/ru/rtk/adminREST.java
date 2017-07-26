011d8d1617ede20c5ed2c917f26c2eb32a6abad9
vasil <andr_vasil@mail.ru>
2017-07-26 22:34:02 +0300

    Рабочая версия

--- userREST/src/main/java/ru/rtk/adminREST.java
+++ userREST/src/main/java/ru/rtk/adminREST.java
@@ -5,12 +5,43 @@
  */
 package ru.rtk;
 
+import beans.user;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
  * @author vasil
  */
 @Path("adm")
 public class adminREST {
+
+    @Context
+    private HttpServletRequest httpRequest;
+    private Logger log = Logger.getLogger(getClass().getName());
     
+     @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public user getText(@PathParam("id") String id) {
+        List<user> res = new ArrayList<>();
+        user u1 = new user(id, "test", "Новый пользователь");
+        log.log(Level.INFO, "header = {0}", httpRequest.getServletPath());
+        log.log(Level.INFO, "header = {0}", httpRequest.getLocalAddr());
+        log.log(Level.INFO, "header = {0}", httpRequest.getMethod());
+        
+
+        
+        //log.info(size);
+        res.add(u1);
+        System.out.println("Test");
+        return u1;
+    }
 }
